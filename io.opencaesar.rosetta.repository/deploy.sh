53486ef8365c03d4f3f3b03eaf0934228e38908b
pogi7 <aaronlevitt7@gmail.com>
2022-12-07 15:11:23 -0800

    configure git in actions

--- io.opencaesar.rosetta.repository/deploy.sh
+++ io.opencaesar.rosetta.repository/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git config user.name "Travis CI"
-git config user.email "melaasar@gmail.com"
+git config user.name "Github Actions"
+git config user.email "aaronlevitt7@gmail.com"
 
 # Now that we're all set up, we can push.
-mvn deploy -Prelease-composite -Dgithub-update-repo=https://x-access-token:${GITHUB_API_KEY}@github.com/opencaesar/oml-rosetta-p2
+mvn deploy -Prelease-composite -Dgithub-update-repo=https://x-access-token:${ACTIONS_TOKEN}@github.com/pogi7/oml-rosetta-p2
